94f87ff89599ff836369da7a6add637325f7e5eb
tmurdoch99 <tmurdoch_99@yahoo.com>
2022-09-06 16:18:02 +0100

    Initial commit

---
+++ app/models/account_invitation.rb
@@ -0,0 +1,70 @@
+# == Schema Information
+#
+# Table name: account_invitations
+#
+#  id            :bigint           not null, primary key
+#  email         :string           not null
+#  name          :string           not null
+#  roles         :jsonb            not null
+#  token         :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  account_id    :integer          not null
+#  invited_by_id :integer
+#
+# Indexes
+#
+#  index_account_invitations_on_account_id     (account_id)
+#  index_account_invitations_on_invited_by_id  (invited_by_id)
+#  index_account_invitations_on_token          (token) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#  fk_rails_...  (invited_by_id => users.id)
+#
+class AccountInvitation < ApplicationRecord
+  ROLES = AccountUser::ROLES
+
+  include Rolified
+
+  belongs_to :account
+  belongs_to :invited_by, class_name: "User", optional: true
+  has_secure_token
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: {scope: :account_id, message: :invited}
+
+  def save_and_send_invite
+    if save
+      AccountInvitationsMailer.with(account_invitation: self).invite.deliver_later
+    end
+  end
+
+  def accept!(user)
+    account_user = account.account_users.new(user: user, roles: roles)
+    if account_user.valid?
+      ApplicationRecord.transaction do
+        account_user.save!
+        destroy!
+      end
+
+      [account.owner, invited_by].uniq.each do |recipient|
+        AcceptedInvite.with(account: account, user: user).deliver_later(recipient)
+      end
+
+      account_user
+    else
+      errors.add(:base, account_user.errors.full_messages.first)
+      nil
+    end
+  end
+
+  def reject!
+    destroy
+  end
+
+  def to_param
+    token
+  end
+end
---
+++ app/models/account_user.rb
@@ -0,0 +1,45 @@
+# == Schema Information
+#
+# Table name: account_users
+#
+#  id         :bigint           not null, primary key
+#  roles      :jsonb            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  account_id :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_account_users_on_account_id  (account_id)
+#  index_account_users_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
+class AccountUser < ApplicationRecord
+  # Add account roles to this line
+  # Do NOT to use any reserved words like `user` or `account`
+  ROLES = [:admin, :member]
+
+  include Rolified
+
+  belongs_to :account
+  belongs_to :user
+
+  validates :user_id, uniqueness: {scope: :account_id}
+  validate :owner_must_be_admin, on: :update, if: -> { admin_changed? && account_owner? }
+
+  def account_owner?
+    account.owner_id == user_id
+  end
+
+  def owner_must_be_admin
+    unless admin?
+      errors.add :admin, :cannot_be_removed
+    end
+  end
+end
